4a8e4fde5d4f77d10d46aad3be93d3886971b224
indra <indra1nkuss@gmail.com>
2026-04-16 11:33:09 +0700

    feat: FASE 2 — Core Constants & Services
    menambahkan beberapa fungsi seperti authentifikasi dan pesan  di app_strings.dart

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,14 @@
+class AppStrings {
+  static const String appName = 'Catalog Inkus';
+
+  // Auth
+  static const String register = 'Daftar';
+  static const String login = 'Masuk';
+  static const String logout = 'Keluar';
+  static const String email = 'Email';
+  static const String password = 'Password';
+
+  // Messages
+  static const String loginSuccess = 'Login berhasil';
+  static const String error = 'Terjadi kesalahan';
+}
